76c1269d8b8496ce91d948a57de9009b83d79b8f
GChimel <gustavochimel12@gmail.com>
2025-04-17 06:11:47 -0300

    feat:

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,4 +3,6 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   entry: ["src/server.ts"],
   outDir: "dist",
+  target: "node18",
+  external: ["@prisma/client", ".prisma/client"],
 });
